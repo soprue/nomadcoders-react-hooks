0bbb26c356af3dd9f510ed50c8ac13ad9645f157
soprue <soprue@naver.com>
2022-11-21 00:38:08 +0900

    Create the example file for useTitle hooks

---
+++ useTitle-example.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+
+  return <div className="App"></div>;
+};
+
+export default App;

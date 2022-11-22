446aad96737386466860422617cb68e639fbc7e3
soprue <soprue@naver.com>
2022-11-22 21:25:43 +0900

    Modify the useConfirm-example.js

--- useConfirm-example.js
+++ useConfirm-example.js
@@ -1,6 +1,6 @@
 const useConfirm = (message = "", onConfirm, onCancle) => {
-  if (onConfirm && typeof onConfirm !== "function") return;
-  if(onCancle && typeof onCancle !== "function") return;
+  if (!onConfirm && typeof onConfirm !== "function") return;
+  if(!onCancle && typeof onCancle !== "function") return;
 
   const confirmAction = () => {
     if (confirm(message)) {

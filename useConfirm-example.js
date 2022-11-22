a6e18c55f3abfcb2433cba8470defdbef6cfcf20
soprue <soprue@naver.com>
2022-11-22 21:27:24 +0900

    Modify the useConfirm-example.js

--- useConfirm-example.js
+++ useConfirm-example.js
@@ -1,27 +1,28 @@
 const useConfirm = (message = "", onConfirm, onCancle) => {
-  if (!onConfirm && typeof onConfirm !== "function") return;
-  if(!onCancle && typeof onCancle !== "function") return;
-
-  const confirmAction = () => {
-    if (confirm(message)) {
+    if (!onConfirm && typeof onConfirm !== "function") return;
+    if (onCancle && typeof onCancle !== "function") return;
+  
+    const confirmAction = () => {
+      if (window.confirm(message)) {
         onConfirm();
-    } else {
+      } else {
         onCancle();
-    }
+      }
+    };
+    return confirmAction;
   };
-  return confirmAction;
-};
-
-const App = () => {
+  
+  const App = () => {
     const deleteWorld = () => console.log("Deleting the world...");
     const abort = () => console.log("Aborted");
     const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
-
+  
     return (
-        <div className="App">
-            <button onClick={confirmDelete}>delete the world</button>
-        </div>
+      <div className="App">
+        <button onClick={confirmDelete}>delete the world</button>
+      </div>
     );
-};
-
-export default App;
+  };
+  
+  export default App;
+  
